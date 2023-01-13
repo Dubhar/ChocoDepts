75a85db6b5304c91bd4d2f3f9c2f28c19b290147
Dubhar <faebhir@gmail.com>
2023-01-13 15:03:59 +0100

    tkinter javascript not working

--- ChocoDepts.py
+++ ChocoDepts.py
@@ -1,81 +1,34 @@
 import glob
-import math
 import os
 import re
-import tkinter
-from operator import attrgetter
-from tkinter import Tk, Canvas, Frame, BOTH, ALL, CENTER
-from defusedxml import ElementTree
 
+import webview
 from colour import Color
+from defusedxml import ElementTree
+from pyvis.network import Network
 
 
-def draw_arrow(canvas, from_x, from_y, to_x, to_y):
-    margin = 70
+def draw_directed_graph(graph, nodes):
+    def get_node_by_id(identifier):
+        matches = [n for n in nodes if n.id == identifier]
+        if len(matches) == 1:
+            return matches[0]
+        print("dependency {} not found/installed/unique!".format(identifier))
 
-    x_abs = abs(to_x - from_x)
-    y_abs = abs(to_y - from_y)
+    # prepare color palette
+    max_connection_count = max(node.connection_count for node in nodes)
+    red = Color("#00FF00")
+    colors = list(red.range_to(Color("#FF0000"), max_connection_count + 1))
 
-    x_margin = margin / (x_abs + y_abs) * x_abs
-    y_margin = margin / (y_abs + x_abs) * y_abs
+    for node in nodes:
+        graph.add_node(node.label, color=colors[node.connection_count].hex)
+        # font = "Sans 12 bold underline" if node.is_leaf else "Sans 12"
 
-    if x_abs > 0:
-        x_direction_correction_factor = (to_x - from_x) / abs(to_x - from_x)
-        to_x -= x_margin * x_direction_correction_factor
-        from_x += x_margin * x_direction_correction_factor
-
-    if y_abs > 0:
-        y_direction_correction_factor = (to_y - from_y) / abs(to_y - from_y)
-        to_y -= y_margin * y_direction_correction_factor
-        from_y += y_margin * y_direction_correction_factor
-
-    canvas.create_line(from_x, from_y, to_x, to_y, arrow=tkinter.LAST)
-
-
-class ChocoDepts(Frame):
-
-    def __init__(self):
-        super().__init__()
-
-        # init ui
-        self.canvas = Canvas(self)
-        self.init_canvas()
-        self.pack(fill=BOTH, expand=1)
-
-    def do_zoom(self, event):
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
-        factor = 1.001 ** event.delta
-        self.canvas.scale(ALL, x, y, factor, factor)
-
-    def init_canvas(self):
-        self.canvas.bind("<MouseWheel>", self.do_zoom)
-        self.canvas.bind('<ButtonPress-1>', lambda event: self.canvas.scan_mark(event.x, event.y))
-        self.canvas.bind("<B1-Motion>", lambda event: self.canvas.scan_dragto(event.x, event.y, gain=1))
-        self.canvas.pack(fill=BOTH, expand=1)
-
-    def draw_directed_graph(self, nodes):
-
-        def get_node_by_id(identifier):
-            matches = [n for n in nodes if n.id == identifier]
-            if len(matches) == 1:
-                return matches[0]
-            print("dependency {} not found/installed/unique!".format(identifier))
-
-        # prepare color palette
-        max_connection_count = max(node.connection_count for node in nodes)
-        red = Color("green")
-        colors = list(red.range_to(Color("red"), max_connection_count + 1))
-
-        for node in nodes:
-            font = "Sans 12 bold underline" if node.is_leaf else "Sans 12"
-            self.canvas.create_text(node.coordinate.x, node.coordinate.y, fill=colors[node.connection_count],
-                                    font=font, text=node.label, justify=CENTER)
-            for dependency in node.dependencies:
-                dependency_node = get_node_by_id(dependency)
-                if dependency_node:
-                    draw_arrow(self.canvas, node.coordinate.x, node.coordinate.y, dependency_node.coordinate.x,
-                               dependency_node.coordinate.y)
+    for node in nodes:
+        for dependency in node.dependencies:
+            dependency_node = get_node_by_id(dependency)
+            if dependency_node:
+                graph.add_edge(node.label, dependency_node.label)
 
 
 class Coordinate:
@@ -120,15 +73,10 @@ class NuspecToNodes:
                     node.dependencies.append(dependency_id)
             self.nodes.append(node)
 
-    def order_nodes_by_dependency_count(self):
-        # add connection count
+    def annotate_nodes_by_relations(self):
         for node in self.nodes:
             node.connection_count = 0
-
-            # outgoing
             node.connection_count += len(node.dependencies)
-
-            # incoming
             node.is_leaf = True
             for other_node in self.nodes:
                 if node == other_node:
@@ -137,27 +85,6 @@ class NuspecToNodes:
                     if dependency == node.id:
                         node.connection_count += 1
                         node.is_leaf = False
-        # sort
-        self.nodes.sort(key=attrgetter('connection_count'), reverse=True)
-
-    def update_node_locations(self):
-        # spiral parameters depend on number of nodes
-        node_count = len(self.nodes)
-        vert_spacing = 10 * node_count
-        horz_spacing = 10 * node_count
-        theta_max = node_count * math.pi
-
-        # constants
-        b = vert_spacing / 2 / math.pi
-        smax = 0.5 * b * theta_max * theta_max
-
-        for i in range(len(self.nodes)):
-            s = (smax / horz_spacing) * i
-            theta_i = math.sqrt(2 * s / b)
-            xi = b * theta_i * math.cos(theta_i)
-            yi = b * theta_i * math.sin(theta_i)
-
-            self.nodes[i].coordinate = Coordinate(xi, yi)
 
     def update_node_labels(self):
         for node in self.nodes:
@@ -165,27 +92,26 @@ class NuspecToNodes:
 
     def get_nodes(self):
         self.read_nodes_from_xml()
-        self.order_nodes_by_dependency_count()
-        self.update_node_locations()
+        self.annotate_nodes_by_relations()
         self.update_node_labels()
         return self.nodes
 
 
-def main():
-    root = Tk()
+def load_html(window):
+    g = Network(height='800px', width='600px')
+    g.toggle_physics(True)
 
-    cd = ChocoDepts()
     nn = NuspecToNodes()
     nodes = nn.get_nodes()
-    cd.draw_directed_graph(nodes)
+    draw_directed_graph(g, nodes)
 
-    window_width = 800
-    window_height = 600
-    x_coordinate = int((root.winfo_screenwidth() / 2) - (window_width / 2))
-    y_coordinate = int((root.winfo_screenheight() / 2) - (window_height / 2))
-    root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_coordinate, y_coordinate))
-    root.title("ChocoDepts v0.0.1")
-    root.mainloop()
+    html = g.generate_html('chocodepts.html')
+    window.load_html(html)
+
+
+def main():
+    window = webview.create_window('ChocoDepts v0.0.1', html='Loading')
+    webview.start(load_html, window)
 
 
 if __name__ == '__main__':
